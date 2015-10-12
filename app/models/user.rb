60e548b07731cb81e50900e6507c2216e53170ac
Douglas <dalindne@gmail.com>
2015-10-12 14:37:53 -0500

    added password check for complexity

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :secure_validatable
 
   validates_presence_of :name, :on=>:create
   validates_presence_of :age, :on=>:create
